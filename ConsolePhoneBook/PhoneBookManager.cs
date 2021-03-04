27e1ff56248ddf9c1828552c4b7478c66fa06b37
Yong Gyu Jeong <yonggyujeong10@gmail.com>
2021-03-04 14:28:06 +0900

    초기 커밋.

---
+++ ConsolePhoneBook/PhoneBookManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhoneBook
+{
+    class PhoneBookManager
+    {
+        const int MAX_CNT = 100;
+        PhoneInfo[] infoStorage = new PhoneInfo[MAX_CNT];
+        int curCnt = 0;
+
+        public void ShowMenu()
+        {
+            Console.WriteLine("-------------------주소록--------------------------");
+            Console.WriteLine("   1.입력 |  2.목록  |  3.검색 |  4.삭제  |  5.종료 ");
+            Console.WriteLine("-------------------주소록--------------------------");
+            Console.WriteLine("선택: ");
+        }
+
+        private int CheckName(string name) // private으로 해도 됨 *****************중요******
+        {
+            // 이름중복체크
+            for (int index = 0; index < curCnt; index++) // 놓쳤다
+            {
+                if (infoStorage[index].Name == name)
+                {
+                    return index; // return 써줘야함 (break 쓰면 전화번호입력으로, return쓰면 다시 반복 처음가서 이름입력받음)
+                }
+            }
+            return -1; // 못찾을 경우 무슨 값을 반환할 거냐
+        }
+
+        public void InputData() 
+        {
+            // 입력값에 대한 유효성을 체크
+            Console.Write("이름: ");
+            string name = Console.ReadLine().Trim(); // 공백 제거
+            if (string.IsNullOrEmpty(name)) // 필수 입력  if (name.Length < 1)--> 선언만 하고 비교를 했을때 에러가 남
+            {
+                Console.WriteLine("이름은 필수 입력입니다.");
+                return; // while문으로 가기?
+            }
+            else
+            {
+                int dataIdx = CheckName(name);
+                if (dataIdx >= 0)
+                {
+                    Console.WriteLine("이미 등록된 이름입니다. 다른이름으로 입력하세요");
+                    return; //다시 돌아라
+                }
+            }
+            
+            Console.Write("전화번호: ");
+            string phone = Console.ReadLine().Trim(); // 공백 제거
+            if (string.IsNullOrEmpty(phone)) // if (name.Length < 1)--> 선언만 하고 비교를 했을때 에러가 남
+            {
+                Console.WriteLine("전화번호는 필수 입력입니다.");
+                return;
+            }
+            Console.WriteLine("생일");
+            string birth = Console.ReadLine().Trim();
+            if (birth.Length < 1)
+            {
+                infoStorage[curCnt++] = new PhoneInfo(name, phone);
+            }
+            else
+            {   
+                infoStorage[curCnt++] = new PhoneInfo(name, phone, birth);
+            }
+        }
+
+        public void ListData()
+        {
+            if (curCnt == 0) //검색결과가 없는건지 오류난건지 구분하기 위해
+            {
+                Console.WriteLine("입력된 주소록이 없습니다.");
+                return; // 밑에 실행되면 안되니까 return!!!
+            }
+
+            for (int i = 0; i < curCnt; i++)
+            {
+                infoStorage[i].ShowPhoneInfo();
+            }
+        }
+
+        public void SearchData()
+        {
+            Console.WriteLine("검색하실 이름: ");
+            string name = Console.ReadLine().Trim();
+            int dataIdx = CheckName(name);
+            if (dataIdx < 0)
+            {
+                Console.WriteLine("검색된 데이터가 없습니다.");
+            }
+            else
+            {
+                infoStorage[dataIdx].ShowPhoneInfo();
+            }
+        }
+
+        public void DeleteData()
+        {
+            Console.WriteLine("삭제하실 이름: ");
+            string name = Console.ReadLine().Trim();
+            int dataIdx = CheckName(name);
+            if (dataIdx < 0)
+            {
+                Console.WriteLine("삭제할 데이터가 없습니다.");
+            }
+            else
+            {
+                for (int i = dataIdx; i < curCnt; i++)
+                {
+                    infoStorage[i] = infoStorage[i + 1];
+                }
+                curCnt--;
+                Console.WriteLine($"{name} 번호가 삭제되었습니다.");
+            }
+
+        }
+
+        public void ToEnd()
+        {
+
+        }
+    }
+}
---
+++ ConsolePhoneBook/PhoneInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhoneBook
+{
+    public class PhoneInfo
+    {
+        string name;            //필수 // private
+        string phoneNumber;     //필수
+        string birth;           //선택
+
+        public string Name { get { return name; } } // set안쓰이니 지우기
+
+        public PhoneInfo(string name, string phoneNumber) // 필수인 부분만 넣어서 하나 만들기
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.birth = null;
+        }
+
+        public PhoneInfo(string name, string phoneNumber, string birth)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.birth = birth;
+        }
+
+        public void ShowPhoneInfo() // 이 메서드를 안만들면 다른 클래스에서 get 할때 모든 prop를 만들어야함
+        {
+            Console.Write("name :" + this.name);
+            Console.Write("\tphone :" + this.phoneNumber);
+            Console.Write("\tbirth :" + this.birth+"\n");
+        }
+
+
+
+    }
+}
